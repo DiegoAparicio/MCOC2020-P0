fa26e02d7c3228ec75ee5150cce65298b28a6d2d
DiegoAparicio <43451947+DiegoAparicio@users.noreply.github.com>
2020-08-14 12:30:24 -0400

    Plotting solvers entrega 5

---
+++ Entrega 5/plotting_entrega5.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 14 11:01:55 2020
+
+@author: 56977
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plotting(names):
+    absisas = [10,20,50,100,200,500,1000,2000,5000,10000,20000]
+    TPO = [0.1e-3,1e-3,1e-2,0.1,1.,10.,60.,60*10]
+    TPO_label = ["0.1 ms", "1 ms", "10 ms", "0.1 s", "1 s", "10 s", "1 min", "10 min"]
+    
+    plt.figure()
+    
+    for name in names:
+        data = np.loadtxt(name)
+        Ns = data[:, 0]
+        dts = data[:, 1]
+        
+        plt.loglog(Ns, dts.T, "-o", label=name)
+        plt.ylabel("Tiempo transcurrido")
+        plt.xlabel("Tamaño matriz N")
+        plt.grid(True)
+        plt.xticks(absisas,absisas,rotation=45)
+        plt.yticks(TPO,TPO_label)
+        
+    plt.tight_layout()
+    plt.legend(loc="upper left")
+    plt.show()
+    
+names = ["A_invB_inv.txt","A_invB_npSolve.txt"]
+plotting(names)
